64c9a080b044fa932c72ffea9b15caf284e72b1c
crazydrummer81 <crazy_drummer@mail.ru>
2020-05-28 23:58:49 +0600

    Исправлены мелкие ошибки CSS

--- plugins/m2slider-simple.js
+++ plugins/m2slider-simple.js
@@ -2,14 +2,34 @@ console.log('m2slider activate');
 let active_slide = undefined;
 m2slider_simple_init();
 
+$(".gallery::before").on("swipeleft",function(){
+  console.log("Вы потащили влево!");
+});
+$(".gallery--thumb").click(function(){
+  showSliderPopup(this);
+})
+
+$(".m2slider--button-close").click(function(){
+  hidePopup();
+})
+$(".m2slider--arrow-left").click(function(){
+  previousSlide();
+})
+$(".m2slider--arrow-right").click(function(){
+  nextSlide();
+})
+// $(".gallery").click(function(){
+//   if( $(window).width() <= 768 ) showSliderPopup(this);
+// })
+
 function m2slider_simple_init() {
   let popup_html = '<div class="m2popup inactive">'+
             '<div class="m2slider">'+
             '<div class="m2slider--content">'+
             '</div>'+
-            '<div class="m2slider--arrow-left">〈</div>'+
-            '<div class="m2slider--arrow-right">〉</div>'+
-            '<div class="m2slider--button-close">⮾</div>'+
+            '<div class="m2slider--arrow-left"><span class="i-arrow-left"></span></div>'+
+            '<div class="m2slider--arrow-right"><span class="i-arrow-right"></span></div>'+
+            '<div class="m2slider--button-close"><span class="i-close"></span></div>'+
             '</div>';
   $('body').append(popup_html);
 }
@@ -32,26 +52,6 @@ function hidePopup(){
   $(".m2popup").addClass("inactive");
   $("body").removeClass("blocked");
 }
-$(".gallery--thumb").click(function(){
-  showSliderPopup(this);
-})
-$(".gallery").click(function(){
-  showSliderPopup(this);
-  console.log('Click on .gallery::before');
-})
-$(".m2slider--button-close").click(function(){
-  hidePopup();
-})
-$(".m2slider--arrow-left").click(function(){
-  previousSlide();
-})
-$(".m2slider--arrow-right").click(function(){
-  nextSlide();
-})
-$(window).on("swipeleft",function(){
-  console.log("Вы потащили влево!");
-});
-
 function nextSlide () {
   if( active_slide.index() == active_slide.parent()[0].children.length-1 ) {
     active_slide = active_slide.siblings().first();
