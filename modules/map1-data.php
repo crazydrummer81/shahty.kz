b75d47f526f398d54de707c51f5fe6cfa602bf44
crazydrummer81 <crazy_drummer@mail.ru>
2020-06-04 16:30:00 +0600

    Вернул старые кнопки слайдера. Увеличил разрешение превьюшек галлереи

--- modules/map1-data.php
+++ modules/map1-data.php
@@ -1,2 +1,7 @@
 <iframe frameborder="no" style="border: 1px solid #f5f5f5; box-sizing: border-box;" width="100%" height="100%" src="https://widgets.2gis.com/widget?type=firmsonmap&amp;options=%7B%22pos%22%3A%7B%22lat%22%3A43.252286%2C%22lon%22%3A76.89156%2C%22zoom%22%3A16%7D%2C%22opt%22%3A%7B%22city%22%3A%22almaty%22%7D%2C%22org%22%3A%2270000001033070815%22%7D"></iframe>
-<div class="map-hand"></div>
+<div class="map-overlay">Прикоснитесь чтобы переместить карту</div>
+<script>
+	$('.map-overlay').on("tap",function(){
+		$('.map-overlay').addClass('minimized');
+	});
+</script>
--- modules/map2-data.php
+++ modules/map2-data.php
@@ -1,2 +1,2 @@
 <iframe frameborder="no" style="border: 1px solid #f5f5f5; box-sizing: border-box;" width="100%" height="100%" src="https://widgets.2gis.com/widget?type=firmsonmap&amp;options=%7B%22pos%22%3A%7B%22lat%22%3A43.336946359872904%2C%22lon%22%3A76.94573163986207%2C%22zoom%22%3A16%7D%2C%22opt%22%3A%7B%22city%22%3A%22almaty%22%7D%2C%22org%22%3A%2270000001031115823%22%7D"></iframe>
-<div class="map-hand"></div>
+<div class="map-overlay"></div>
