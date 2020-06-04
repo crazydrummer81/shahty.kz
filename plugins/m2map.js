25074717ffae51a580b3c376a484993db9c965ed
crazydrummer81 <crazy_drummer@mail.ru>
2020-06-05 01:11:10 +0600

    Доработан сворачивающийся оверлей для карты

--- plugins/m2map.js
+++ plugins/m2map.js
@@ -2,22 +2,19 @@ $( document ).ready(function() {
 	console.log('PIZDA');
 	
 	$(".m2map-overlay").addClass('m2map-maximized');
-	$(".m2map-overlay").text('Прикоснитесь, чтобы переместить карту');
 	
 	$(".m2map-overlay").click(function() {
 		console.log("MAX");
 		if($(this).hasClass('m2map-maximized')) {
-			$(this).animate({ width:"50px", height:"50px", borderTopRightRadius:"15px",opacity:0.5 },"easein",
+			$(this).animate({ width:"50px", height:"50px", borderTopRightRadius:"15px" },"easein",
 				function() {
-					$(this).text('');
 					$(this).removeClass('m2map-maximized');
 					$(this).addClass('m2map-minimized');
 					$(this).text('');
 				});
 		} else {
-			$(this).animate({ width:"100%", height:"100%", borderTopRightRadius:0,opacity:0.3 },"easein",
+			$(this).animate({ width:"100%", height:"100%", borderTopRightRadius:0 },"easein",
 				function() {
-					$(this).text('Прикоснитесь, чтобы переместить карту');
 					if ($(this).hasClass('m2map-minimized')) {
 						$(this).removeClass('m2map-minimized');
 						$(this).addClass('m2map-maximized');
